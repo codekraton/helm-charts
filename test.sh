40fa09f8dbecf10811fb8e0b024e1da6cfc9ab0e
tkaviani <thomas@codekraton.be>
2025-08-14 20:08:34 +0200

    remove certs

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 helm upgrade \
   --install kubernetes-dashboard ./codekraton-kubernetes-dashboard \
   -f ./codekraton-kubernetes-dashboard/values.yaml \
-  --namespace kubernetes-dashboard --create-namespace \
-  --set certificateKey="$(cat ./codekraton-kubernetes-dashboard/certs/dashboard.key)" \
-  --set certificate="$(cat ./codekraton-kubernetes-dashboard/certs/dashboard.crt)" --wait
+  --namespace kubernetes-dashboard --create-namespace --wait
